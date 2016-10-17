57ae355264be350a1eeb2f341f4a676308c95d13
Riccardo Ressi <Riccardo.Ressi@gmail.com>
2016-10-17 06:15:41 +0200

    Tokenizer now also dedups tokens

--- src/github.com/rressi/smartsearch/tokenizer.go
+++ src/github.com/rressi/smartsearch/tokenizer.go
@@ -12,15 +12,29 @@ func Tokenize(query string) (tokens []string) {
 		return
 	}
 
+	// Normalizes the query:
 	var buf bytes.Buffer
 	buf.ReadFrom(ReadNormalized(bytes.NewBufferString(query)))
 
+	// Extracts all non-empty tokens:
 	for _, token := range strings.Split(buf.String(), " ") {
 		if len(token) > 0 {
 			tokens = append(tokens, token)
 		}
 	}
-	sort.Strings(tokens)
+
+	// Sorts and deduplicates extracted tokens:
+	if len(tokens) > 1 {
+		sort.Strings(tokens)
+		i := 0
+		for j := 1; j < len(tokens); j++ {
+			if tokens[i] != tokens[j] {
+				i++
+				tokens[i] = tokens[j]
+			}
+		}
+		tokens = tokens[:i+1]
+	}
 
 	return
 }
--- src/github.com/rressi/smartsearch/tokenizer_test.go
+++ src/github.com/rressi/smartsearch/tokenizer_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestTokenizer_Base(t *testing.T) {
 
-	tokens := Tokenize("YES!-This ìs ä fÄncy,  string")
+	tokens := Tokenize("YES!-This ìs ä fÄncy, is a string")
 	expected_tokens := []string{"a", "fancy", "is", "string", "this", "yes"}
 	if !reflect.DeepEqual(tokens, expected_tokens) {
 		t.Errorf("Unexpected result: tockens=%v", tokens)
