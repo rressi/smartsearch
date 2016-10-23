18cb67dfe6a2089d078318d66e5eea21ad661e1b
Riccardo Ressi <Riccardo.Ressi@gmail.com>
2016-10-23 10:52:11 +0100

    Support for multiple functional tests added

--- test.sh
+++ test.sh
@@ -7,4 +7,19 @@ go fmt   src/makeindex.go                 || exit 1
 go build src/makeindex.go                 || exit 1
 go fmt   src/searchservice.go             || exit 1
 go build src/searchservice.go             || exit 1
-python   test/functional_test.py          || exit 1
+
+num_failed=0
+for test_script in test/test_*.py
+do
+    echo
+    echo "---------------------------------------------------------------------"
+    echo "Running $test_script..."
+    python ${test_script} || num_failed=$(expr ${num_failed} + 1)
+done
+
+echo "---------------------------------------------------------------------"
+if [ "${num_failed}" -eq "0" ]; then
+   echo "Success!";
+else
+   echo "${num_failed} failed tests";
+fi
