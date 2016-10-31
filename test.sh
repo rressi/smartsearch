f7e6dc08c8826e9efab853a3fbde53772b20d5ec
Riccardo Ressi <Riccardo.Ressi@gmail.com>
2016-10-31 21:47:51 +0100

    First attempt with TravisCI

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export GOPATH=$(pwd)
-go fmt  github.com/rressi/smartsearch     || exit 1
+go fmt   github.com/rressi/smartsearch    || exit 1
 go test  github.com/rressi/smartsearch -v || exit 1
 go fmt   src/makeindex.go                 || exit 1
 go build src/makeindex.go                 || exit 1
